241fc18f20fd395f14ad2323e009dfb9ad67b610
averageandyyy <averageandyyy@gmail.com>
2024-10-06 13:01:55 +0800

    Update Parser to only take in arguments enclosed in double quotes - Parser will check the argument following each option to be enclosed in double quotes and will throw an exception otherwise - Introduce Options class to streamline addition of future options - Introduce initalizeOptions method in Parser to iteratively add options to Parser

--- src/main/java/constants/Regex.java
+++ src/main/java/constants/Regex.java
@@ -1,5 +1,10 @@
 package constants;
 
+import java.util.regex.Pattern;
+
 public class Regex {
     public static final String SPACE_REGEX = " ";
+
+    /** Pattern to match quoted strings or individual words */
+    public static final Pattern USER_COMMAND_REGEX = Pattern.compile("\"[^\"]*\"|\\S+");
 }
