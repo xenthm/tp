53268833dde74ad214614277081855e63b93810f
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-10-13 03:54:38 +0800

    Add logging to deleteAuthor

--- src/main/java/commands/DeleteAuthorCommand.java
+++ src/main/java/commands/DeleteAuthorCommand.java
@@ -4,11 +4,13 @@ import author.Author;
 import author.AuthorList;
 import exceptions.TantouException;
 import ui.Ui;
+import java.util.logging.*;
 
 import static constants.Command.DELETE_COMMAND;
 
 public class DeleteAuthorCommand extends Command {
     private String userInput;
+    private static Logger logger = Logger.getLogger(DeleteAuthorCommand.class.getName());
 
     public DeleteAuthorCommand(String userInput) {
         super(DELETE_COMMAND);
@@ -24,9 +26,11 @@ public class DeleteAuthorCommand extends Command {
         String authorName = parser.getAuthorNameFromInput(userInput);
 
         if (authorName.isEmpty()) {
+            logger.log(Level.SEVERE, "Author name cannot be empty");
             throw new TantouException("No author provided!");
         }
         assert !authorName.isEmpty(): "Author name is empty";
+        logger.log(Level.INFO, "Deleting author... " + authorName);
 
         Author deletingAuthor = new Author(authorName);
 
@@ -34,9 +38,11 @@ public class DeleteAuthorCommand extends Command {
             authorList.deleteAuthor(deletingAuthor);
             System.out.print("Bye bye~");
             System.out.printf("Successfully deleted author: %s\n", deletingAuthor.getAuthorName());
+            logger.log(Level.INFO, "Successfully deleted author: " + deletingAuthor.getAuthorName());
             return;
         }
         assert !authorList.hasAuthor(deletingAuthor): "Author not found";
+        logger.log(Level.SEVERE, "Author not found");
 
         throw new TantouException("Author does not exist!");
     }
