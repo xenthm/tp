5ac2fbadddaa2a2bc1c2ca14a40569d8d935a35b
xenthm <56202777+xenthm@users.noreply.github.com>
2024-10-20 12:21:37 +0800

    Move Storage usage from Main into Tantou class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-import author.AuthorList;
-import storage.Storage;
 import tantou.Tantou;
 
 public class Main {
     public static void main(String[] args) {
         Tantou tantou = new Tantou();
-        AuthorList existingList = Storage.getInstance().readAuthorListFromDataFile();
-        if (existingList != null) {
-            tantou.setAuthorList(existingList);
-        }
         tantou.run();
     }
 }
--- src/main/java/tantou/Tantou.java
+++ src/main/java/tantou/Tantou.java
@@ -4,6 +4,7 @@ import author.AuthorList;
 import commands.Command;
 import exceptions.TantouException;
 import parser.Parser;
+import storage.Storage;
 import ui.Ui;
 
 public class Tantou {
@@ -24,6 +25,11 @@ public class Tantou {
     }
 
     public void run() {
+        AuthorList existingList = Storage.getInstance().readAuthorListFromDataFile();
+        if (existingList != null) {
+            setAuthorList(existingList);
+        }
+
         ui.greetUser();
 
         while (!isExit) {
