d9aff04f4005c94c9e5ba6928d147ac15095e865
xenthm <56202777+xenthm@users.noreply.github.com>
2024-10-20 13:29:43 +0800

    Extract AuthorTest Author creation

--- src/test/java/author/AuthorTest.java
+++ src/test/java/author/AuthorTest.java
@@ -9,36 +9,42 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AuthorTest {
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream STANDARD_OUT = System.out;
+    private final ByteArrayOutputStream OUTPUT_STREAM_CAPTOR = new ByteArrayOutputStream();
+
+    private final Author AUTHOR_WITH_NO_MANGAS = new Author("AUTHOR_WITH_NO_MANGAS");
+
+    private final Author AUTHOR_WITH_TWO_MANGAS = new Author("AUTHOR_WITH_TWO_MANGAS") {
+        // instance initializer for anonymous subclass of Author
+        {
+            addManga(new Manga("manga1", this));
+            addManga(new Manga("manga2", this));
+        }
+    };
 
     @Test
     public void printMangaListFromAuthor_twoMangas() {
-        System.setOut(new PrintStream(outputStreamCaptor));
+        System.setOut(new PrintStream(OUTPUT_STREAM_CAPTOR));
         try {
-            Author author = new Author("test1");
-            author.addManga(new Manga("manga1", author));
-            author.addManga(new Manga("manga2", author));
-            assertEquals(2, author.getMangaList().size());
-            author.printMangaList();
+            assertEquals(2, AUTHOR_WITH_TWO_MANGAS.getMangaList().size());
+            AUTHOR_WITH_TWO_MANGAS.printMangaList();
             assertEquals("1. manga1 | Deadline: None" + System.lineSeparator()
                             + "2. manga2 | Deadline: None" + System.lineSeparator(),
-                    outputStreamCaptor.toString());
+                    OUTPUT_STREAM_CAPTOR.toString());
         } finally {
-            System.setOut(standardOut);
+            System.setOut(STANDARD_OUT);
         }
     }
 
     @Test
     public void printMangaListFromAuthor_noMangas() {
-        System.setOut(new PrintStream(outputStreamCaptor));
+        System.setOut(new PrintStream(OUTPUT_STREAM_CAPTOR));
         try {
-            Author author = new Author("test2");
-            assertEquals(0, author.getMangaList().size());
-            author.printMangaList();
-            assertEquals("",  outputStreamCaptor.toString());
+            assertEquals(0, AUTHOR_WITH_NO_MANGAS.getMangaList().size());
+            AUTHOR_WITH_NO_MANGAS.printMangaList();
+            assertEquals("", OUTPUT_STREAM_CAPTOR.toString());
         } finally {
-            System.setOut(standardOut);
+            System.setOut(STANDARD_OUT);
         }
     }
 }
