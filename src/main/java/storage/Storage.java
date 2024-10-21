c93350eb19663c5682295a5e13e781332e9916c5
xenthm <56202777+xenthm@users.noreply.github.com>
2024-10-21 18:25:31 +0800

    Decouple methods in Storage class
    - Remove check for file location in readAuthorListFromDataFile()
      method, using FileReader exceptions to handle
    - Add check for file location into saveAuthorListToDateFile() to ensure
      that every call to it checks the location.
    - Add JavaDoc to the new createFileIfNeeded() method

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -35,32 +35,13 @@ public class Storage {
     private Storage() {
         assert DATA_PATH.endsWith(".json") : "data file path should be of type .json";
         logger = Logger.getLogger(this.getClass().getName());
-        setupDataFile();
+        dataFile = new File(DATA_PATH);
+        logger.info("Data file path initialized to: " + dataFile.getAbsolutePath());
         gson = new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
                 .setPrettyPrinting()
                 .registerTypeAdapter(Author.class, new AuthorDeserializer())
                 .create();
-    }
-
-    private void setupDataFile() {
-        dataFile = new File(DATA_PATH);
-        logger.info("Data file path initialized to: " + dataFile.getAbsolutePath());
-        try {
-            // check with short-circuiting if path has a parent directory,
-            // then if the directories were created with mkdirs()
-            if (dataFile.getParentFile() != null && dataFile.getParentFile().mkdirs()) {
-                logger.info("Directories for data file created");
-            }
-
-            // check if data file was created
-            if (dataFile.createNewFile()) {
-                logger.info("Data file created");
-            }
-        } catch (IOException | SecurityException e) {
-            // TODO: need to handle app operation when data cannot be saved!
-            logger.warning("Problems setting up data file, data will not be saved!" + e.getMessage());
-        }
     }
 
     public Gson getGson() {
@@ -92,13 +73,38 @@ public class Storage {
             logger.info("Data restored");
             return authorList;
         } catch (IOException e) {
-            logger.warning("Problems reading file, data was not restored!" + e.getMessage());
+            logger.warning("Problems accessing file, data was not restored!" + e.getMessage());
+        } catch (JsonParseException e) {
+            logger.warning("Problems parsing JSON from file, data was not restored!" + e.getMessage());
         }
         return null;
     }
 
+    /**
+     * Checks if the data storage file <code>dataFile</code> can be found at its specified location. If not, creates
+     * the corresponding directories and file, logging the process.
+     */
+    private void createFileIfNeeded() {
+        assert dataFile != null : "dataFile cannot be null";
+        try {
+            // check with short-circuiting if path has a parent directory,
+            // then if the directories were created with mkdirs()
+            if (dataFile.getParentFile() != null && dataFile.getParentFile().mkdirs()) {
+                logger.info("Directories not found; created them");
+            }
+
+            // check if data file was created
+            if (dataFile.createNewFile()) {
+                logger.info("Data file not found; created it");
+            }
+        } catch (IOException | SecurityException e) {
+            logger.warning("Problems creating data file, data will not be saved!" + e.getMessage());
+        }
+    }
+
     public void saveAuthorListToDataFile(AuthorList authorList) {
         assert authorList != null : "authorList cannot be null";
+        createFileIfNeeded();
         try (FileWriter writer = new FileWriter(dataFile)) {
             gson.toJson(authorList, writer);
             logger.info("Data saved");
