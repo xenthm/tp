86127829256d2257ec0c9f5c67b3dff77016b9b8
xenthm <56202777+xenthm@users.noreply.github.com>
2024-10-21 17:26:20 +0800

    Add JavaDoc to AuthorList class

--- src/main/java/author/AuthorList.java
+++ src/main/java/author/AuthorList.java
@@ -9,6 +9,13 @@ public class AuthorList extends ArrayList<Author> {
     }
 
     //@@author xenthm
+    /**
+     * Checks if the <code>authorList</code> contains an author instance with the name given by the parameter
+     * <code>authorName</code> in <code>String</code> format.
+     *
+     * @param authorName the author's name in <code>String</code> format
+     * @return <code>true</code> if the author was found, <code>false</code> otherwise
+     */
     public boolean hasAuthor(String authorName) {
         assert !authorName.isEmpty() : "author name must not be empty";
         for (Author author : this) {
@@ -21,14 +28,11 @@ public class AuthorList extends ArrayList<Author> {
 
     //@@author averageandyyy
     /**
-     * Searches for existing author in {@code authorList} based on incoming author's
-     * name
-     * and returns {@code Author} instance in {@code authorList}
+     * Searches for existing author in {@code authorList} based on incoming author's name and returns {@code Author}
+     * instance in {@code authorList}
      *
      * @param author The incoming author to search for based on name
-     * @return {@code Author} instance in {@code authorList}, {@code null} if no
-     *         such author
-     *         can be found
+     * @return {@code Author} instance in {@code authorList}, {@code null} if no such author can be found
      */
     public Author getAuthor(Author author) {
         assert author != null : "author must not be null";
@@ -42,6 +46,13 @@ public class AuthorList extends ArrayList<Author> {
     }
 
     //@@author xenthm
+    /**
+     * Searches for existing author in {@code authorList} based on incoming author's name (in <code>String</code>
+     * format) and returns {@code Author} instance in {@code authorList}
+     *
+     * @param authorName the author's name in <code>String</code> format
+     * @return {@code Author} instance in {@code authorList}, {@code null} if no such author can be found
+     */
     public Author getAuthor(String authorName) {
         assert !authorName.isEmpty() : "author name must not be empty";
         for (Author a : this) {
@@ -52,6 +63,9 @@ public class AuthorList extends ArrayList<Author> {
         return null;
     }
 
+    /**
+     * Prints the entire <code>AuthorList</code> after formatting.
+     */
     public void print() {
         assert size() >= 0 : "AuthorList.size() must be non-negative";
 
