4df457463f7d3f81d5bd49084b550b7bf09c4eac
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-10-21 17:18:27 +0800

    Add sales to current list of commands

--- src/main/java/constants/Command.java
+++ src/main/java/constants/Command.java
@@ -6,6 +6,7 @@ public class Command {
     public static final String CATALOG_COMMAND = "catalog";
     public static final String VIEW_COMMAND = "view";
     public static final String DELETE_COMMAND = "delete";
+    public static final String SALES_COMMAND = "sales";
 
     public static final int COMMAND_INDEX = 0;
 }
