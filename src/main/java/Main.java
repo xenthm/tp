67a773bb5ceecf51bfe4615312cecff48c6e4d9c
xenthm <56202777+xenthm@users.noreply.github.com>
2024-10-29 23:13:28 +0800

    Add gradle task to run Tantou without logging
    - Added arg check in Main()
    - Add gradle task "nolog" to build.gradle file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import tantou.Tantou;
 
+import java.util.logging.LogManager;
+
 public class Main {
     public static void main(String[] args) {
         Tantou tantou = new Tantou();
+        if (args.length != 0 && args[0].equals("nolog")) {
+            LogManager.getLogManager().reset();
+        }
         tantou.run();
     }
 }
