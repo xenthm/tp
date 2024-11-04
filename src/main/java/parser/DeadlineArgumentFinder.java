d99e4f527449a84a8683744450dd99074e9b27fe
averageandyyy <averageandyyy@gmail.com>
2024-11-04 14:26:15 +0800

    Remove empty deadline check from DeadlineArgumentFinder

--- src/main/java/parser/DeadlineArgumentFinder.java
+++ src/main/java/parser/DeadlineArgumentFinder.java
@@ -14,15 +14,12 @@ public class DeadlineArgumentFinder extends ArgumentFinder {
     @Override
     public ArgumentResult getArgumentResult(String userInput) throws TantouException {
         String deadline = null;
-        String outputString = null;
+        String outputString = userInput;
         matcher = pattern.matcher(userInput);
 
         if (matcher.find()) {
             deadline = matcher.group(0).trim();
-            if (deadline.isEmpty()) {
-                throw new NoDeadlineProvidedException();
-            }
-
+            
             outputString =
                     userInput.substring(0, matcher.start(0) - 3) + userInput.substring(matcher.end(0));
         }
