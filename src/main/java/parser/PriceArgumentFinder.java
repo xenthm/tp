190b6b385c12698d6b502073a78882be6b605ac5
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-11-07 22:43:28 +0800

    Update JavaDoc comments for ArgumentFinders

--- src/main/java/parser/PriceArgumentFinder.java
+++ src/main/java/parser/PriceArgumentFinder.java
@@ -4,6 +4,17 @@ package parser;
 import static constants.Options.PRICE_OPTION;
 import static constants.Regex.PRICE_EXTRACTOR_PATTERN;
 
+/**
+ * A concrete implementation of {@link ArgumentFinder} that extracts a price
+ * from a user input string based on a predefined pattern.
+ *
+ * <p>The {@code PriceArgumentFinder} class applies a regular expression pattern to
+ * locate and extract the price from the input string. If a price is
+ * successfully matched, it removes the matched portion and any associated flag
+ * from the input, returning an {@link ArgumentResult} with both the extracted price
+ * and the remaining input. If no match is found, an {@code ArgumentResult} is returned
+ * with a {@code null} price.
+ */
 public class PriceArgumentFinder extends ArgumentFinder {
 
     public PriceArgumentFinder() {
--- src/main/java/parser/QuantityArgumentFinder.java
+++ src/main/java/parser/QuantityArgumentFinder.java
@@ -4,6 +4,17 @@ package parser;
 import static constants.Options.QUANTITY_OPTION;
 import static constants.Regex.QUANTITY_EXTRACTOR_PATTERN;
 
+/**
+ * A concrete implementation of {@link ArgumentFinder} that extracts a quantity
+ * from a user input string based on a predefined pattern.
+ *
+ * <p>The {@code QuantityArgumentFinder} class applies a regular expression pattern to
+ * locate and extract the quantity from the input string. If a price is
+ * successfully matched, it removes the matched portion and any associated flag
+ * from the input, returning an {@link ArgumentResult} with both the extracted quantity
+ * and the remaining input. If no match is found, an {@code ArgumentResult} is returned
+ * with a {@code null} quantity.
+ */
 public class QuantityArgumentFinder extends ArgumentFinder {
     public QuantityArgumentFinder() {
         super(QUANTITY_EXTRACTOR_PATTERN, QUANTITY_OPTION);
