74bfc769abc16c746e501c02bd7f568df7568d20
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-11-07 22:52:01 +0800

    Remove unused exceptions

--- src/main/java/exceptions/MangaArgsWrongOrderException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class MangaArgsWrongOrderException extends TantouException {
-    public static final String MANGA_ARGUMENTS_WRONG_ORDER_EXCEPTION = "Specify the author before the manga!";
-    public MangaArgsWrongOrderException() {
-        super(MANGA_ARGUMENTS_WRONG_ORDER_EXCEPTION);
-    }
-}
