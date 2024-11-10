ed2cf18f9c6741a1a0fc1ffa49bb689cd1a1f3a5
xenthm <56202777+xenthm@users.noreply.github.com>
2024-11-10 23:30:30 +0800

    Extract validity checks from Command subclasses
    Created the CommandValidity utility class.

---
+++ src/main/java/commands/CommandValidity.java
@@ -0,0 +1,196 @@
+package commands;
+
+import author.Author;
+import author.AuthorList;
+import exceptions.AuthorDoesNotExistException;
+import exceptions.AuthorExistsException;
+import exceptions.AuthorListEmptyException;
+import exceptions.AuthorNameTooLongException;
+import exceptions.MangaDoesNotExistException;
+import exceptions.MangaExistsException;
+import exceptions.MangaListEmptyException;
+import exceptions.MangaNameTooLongException;
+import exceptions.NoAuthorProvidedException;
+import exceptions.NoMangaProvidedException;
+import exceptions.NoPriceProvidedException;
+import exceptions.NoQuantityProvidedException;
+
+import static commands.Command.COMMAND_LOGGER;
+import static constants.Options.MAX_AUTHOR_NAME_LENGTH;
+import static constants.Options.MAX_MANGA_NAME_LENGTH;
+
+//@@author xenthm
+/**
+ * This utility class contains methods to check the various inputs to the defined <code>Command</code>s in
+ * <code>MangaTantou</code>, throwing relevant exceptions if needed.
+ */
+public class CommandValidity {
+    /**
+     * Throws <code>NoAuthorProvidedException</code> if author is <code>null</code> or is empty.
+     *
+     * @param authorName The author name <code>String</code> to be checked.
+     */
+    public static void checkAuthorProvided(String authorName) throws NoAuthorProvidedException {
+        if (authorName == null || authorName.isEmpty()) {
+            if (authorName == null) {
+                COMMAND_LOGGER.warning("Provided author name is null");
+            } else {
+                COMMAND_LOGGER.warning("Provided author name is empty");
+            }
+            throw new NoAuthorProvidedException();
+        }
+    }
+
+    /**
+     * Throws <code>NoMangaProvidedException</code> if manga is <code>null</code> or is empty.
+     *
+     * @param mangaName The manga name <code>String</code> to be checked.
+     */
+    public static void checkMangaProvided(String mangaName) throws NoMangaProvidedException {
+        if (mangaName == null || mangaName.isEmpty()) {
+            if (mangaName == null) {
+                COMMAND_LOGGER.warning("Provided manga name is null");
+            } else {
+                COMMAND_LOGGER.warning("Provided manga name is empty");
+            }
+            throw new NoMangaProvidedException();
+        }
+    }
+
+    /**
+     * Throws <code>NoQuantityProvidedException</code> if quantity is <code>null</code> or is empty.
+     *
+     * @param quantityString The quantity <code>String</code> to be checked.
+     */
+    public static void checkQuantityProvided(String quantityString) throws NoQuantityProvidedException {
+        if (quantityString == null || quantityString.isEmpty()) {
+            if (quantityString == null) {
+                COMMAND_LOGGER.warning("Provided quantity is null");
+            } else {
+                COMMAND_LOGGER.warning("Provided quantity is empty");
+            }
+            throw new NoQuantityProvidedException();
+        }
+    }
+
+    /**
+     * Throws <code>NoPriceProvidedException</code> if price is <code>null</code> or is empty.
+     *
+     * @param priceString The price <code>String</code> to be checked.
+     */
+    public static void checkPriceProvided(String priceString) throws NoPriceProvidedException {
+        if (priceString == null || priceString.isEmpty()) {
+            if (priceString == null) {
+                COMMAND_LOGGER.warning("Provided price is null");
+            } else {
+                COMMAND_LOGGER.warning("Provided price is empty");
+            }
+            throw new NoPriceProvidedException();
+        }
+    }
+
+    /**
+     * Throws <code>AuthorNameTooLongException</code> if author name is longer than MAX_AUTHOR_NAME_LENGTH.
+     *
+     * @param authorName The manga name <code>String</code> to be checked.
+     */
+    public static void checkAuthorNameLength(String authorName) throws AuthorNameTooLongException {
+        if (authorName.length() > MAX_AUTHOR_NAME_LENGTH) {
+            COMMAND_LOGGER.warning("Provided author name \"" + authorName + "\" exceeds maximum length at "
+                    + authorName.length());
+            throw new AuthorNameTooLongException();
+        }
+    }
+
+    /**
+     * Throws <code>MangaNameTooLongException</code> if manga name is longer than MAX_MANGA_NAME_LENGTH.
+     *
+     * @param mangaName The manga name <code>String</code> to be checked.
+     */
+    public static void checkMangaNameLength(String mangaName) throws MangaNameTooLongException {
+        if (mangaName.length() > MAX_MANGA_NAME_LENGTH) {
+            COMMAND_LOGGER.warning("Provided manga name \"" + mangaName + "\" exceeds maximum length at "
+                    + mangaName.length());
+            throw new MangaNameTooLongException();
+        }
+    }
+
+    /**
+     * Throws <code>AuthorExistsException</code> if author already exists in the <code>authorList</code>.
+     *
+     * @param authorName The author name <code>String</code> to be checked.
+     * @param authorList The <code>AuthorList</code> to search.
+     */
+    public static void checkDuplicateAuthor(String authorName, AuthorList authorList) throws AuthorExistsException {
+        if (authorList.hasAuthor(authorName)) {
+            COMMAND_LOGGER.warning("\"" + authorName + "\" already exists in the author list");
+            throw new AuthorExistsException();
+        }
+    }
+
+    /**
+     * Throws <code>MangaExistsException</code> if manga already exists in the <code>mangaList</code> of the author.
+     *
+     * @param mangaName The manga name <code>String</code> to be checked.
+     * @param author    The <code>Author</code> whose <code>mangaList</code> to search.
+     */
+    public static void checkDuplicateManga(String mangaName, Author author) throws MangaExistsException {
+        if (author.hasManga(mangaName)) {
+            COMMAND_LOGGER.warning("\"" + mangaName + "\" already exists in the manga list of \""
+                    + author.getAuthorName() + "\"");
+            throw new MangaExistsException();
+        }
+    }
+
+    /**
+     * Throws <code>AuthorDoesNotExistException</code> if author cannot be found in the <code>authorList</code>.
+     *
+     * @param authorName The author name <code>String</code> to be checked.
+     * @param authorList The <code>AuthorList</code> to search.
+     */
+    public static void checkAuthorDoesNotExist(String authorName, AuthorList authorList) throws AuthorDoesNotExistException {
+        if (!authorList.hasAuthor(authorName)) {
+            COMMAND_LOGGER.warning("\"" + authorName + "\" does not exist in the author list");
+            throw new AuthorDoesNotExistException(authorName);
+        }
+    }
+
+    /**
+     * Throws <code>MangaDoesNotExistException</code> if manga cannot be found in the <code>mangaList</code> of the
+     * author.
+     *
+     * @param mangaName The manga name <code>String</code> to be checked.
+     * @param author    The <code>Author</code> whose <code>mangaList</code> to search.
+     */
+    public static void checkAuthorDoesNotExist(String mangaName, Author author) throws MangaDoesNotExistException {
+        if (!author.hasManga(mangaName)) {
+            COMMAND_LOGGER.warning("\"" + mangaName + "\" does not exist in the manga list of \""
+                    + author.getAuthorName() + "\"");
+            throw new MangaDoesNotExistException(mangaName);
+        }
+    }
+
+    /**
+     * Throws <code>AuthorListEmptyException</code> if the <code>authorList</code> is empty.
+     *
+     * @param authorList The <code>AuthorList</code> to be checked.
+     */
+    public static void checkAuthorListNotEmpty(AuthorList authorList) throws AuthorListEmptyException {
+        if (authorList.isEmpty()) {
+            COMMAND_LOGGER.info("The author list is empty");
+            throw new AuthorListEmptyException();
+        }
+    }
+
+    /**
+     * Throws <code>MangaListEmptyException</code> if the <code>mangaList</code> of the author is empty.
+     *
+     * @param author The <code>Author</code> whose <code>mangaList</code> is to be checked.
+     */
+    public static void checkMangaListNotEmpty(Author author) throws MangaListEmptyException {
+        if (author.getMangaList().isEmpty()) {
+            COMMAND_LOGGER.info("The author list is empty");
+            throw new MangaListEmptyException(author.getAuthorName());
+        }
+    }
+}
