2a980efcdddf3638a4b426b8344ca22230eee7d8
xenthm <56202777+xenthm@users.noreply.github.com>
2024-11-11 03:21:39 +0800

    Use constants package to print message in exceptions
    - PriceTooLargeException: MAX_UNIT_PRICE_VALUE
    - QuantityTooLargeException: MAX_QUANTITY_VALUE

--- src/main/java/exceptions/QuantityTooLargeException.java
+++ src/main/java/exceptions/QuantityTooLargeException.java
@@ -1,8 +1,10 @@
 package exceptions;
 
+import static constants.Options.MAX_QUANTITY_VALUE;
+
 public class QuantityTooLargeException extends TantouException {
     public static final String QUANTITY_TOO_LARGE_EXCEPTION = "Woah woah woah... \n" +
-            "We aren't THAT successful. Try something below 1000000000.";
+            "We aren't THAT successful. Try something below " + MAX_QUANTITY_VALUE + ".";
     public QuantityTooLargeException() {
         super(QUANTITY_TOO_LARGE_EXCEPTION);
     }
