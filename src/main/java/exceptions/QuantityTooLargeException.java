10d8954f168efea5a9846524668e380c0caddccb
xenthm <56202777+xenthm@users.noreply.github.com>
2024-11-10 19:42:39 +0800

    Refactor exceptions to use extracted constants

--- src/main/java/exceptions/QuantityTooLargeException.java
+++ src/main/java/exceptions/QuantityTooLargeException.java
@@ -1,8 +1,10 @@
 package exceptions;
 
+import static constants.Options.QUANTITY_MAX_VALUE;
+
 public class QuantityTooLargeException extends TantouException {
     public static final String QUANTITY_TOO_LARGE_EXCEPTION = "Woah woah woah... \n" +
-            "We aren't THAT successful. Try something below 1000000000.";
+            "We aren't THAT successful. Try something below " + QUANTITY_MAX_VALUE + ".";
     public QuantityTooLargeException() {
         super(QUANTITY_TOO_LARGE_EXCEPTION);
     }
